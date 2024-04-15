0c441fca679e54d781386cf4264be364e738e9aa
quabwww <josuebuenovargas@gmail.com>
2024-04-15 12:12:27 -0500

    v2

--- main.py
+++ main.py
@@ -27,6 +27,3 @@ router_principal = APIRouter()
 registrar_rutas_desde_directorio(router_principal, carpeta_api)
 app.include_router(router_principal)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
